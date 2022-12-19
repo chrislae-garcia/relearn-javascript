e6ca416c9caf51482ef30dbb78aa4362f785378f
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-19 19:01:34 +0800

    Add logic to reverse strings

--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(rawString) {
 
+  let reversedString = '';
+
+  for (let i = 1; i <= rawString.length; i++){
+    reversedString = reversedString + rawString.substr(-i, 1);
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
