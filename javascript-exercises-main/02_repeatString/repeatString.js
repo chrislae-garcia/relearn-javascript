0b0796decb3bf92070080c034a3295d480ad272b
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-19 18:32:33 +0800

    Add logic to repeat strings

--- javascript-exercises-main/02_repeatString/repeatString.js
+++ javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(rawString, numberOfRepeats) {
+  let repeatedString = '';
 
+  if (numberOfRepeats < 0) return 'ERROR';
+
+  for (i = 0; i < numberOfRepeats; i++) {
+    repeatedString = repeatedString + rawString;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
