e6f80afeaf5c1548e7aa85310c578934b66479b5
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-20 11:57:16 +0800

    Add logic and validation for function sumAll()

--- javascript-exercises-main/05_sumAll/sumAll.js
+++ javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstInt, lastInt) {
 
+  if ((firstInt < 0 || lastInt < 0)
+    || (typeof firstInt != 'number')
+    || (typeof lastInt != 'number'))
+    return 'ERROR';
+
+  let smallInt = firstInt;
+  let largeInt = lastInt;
+  let temp = 0;
+  let sum = 0;
+
+  if (smallInt > largeInt) {
+    temp = smallInt;
+    smallInt = largeInt;
+    largeInt = temp;
+  }
+
+  for (i = smallInt; i <= largeInt; i++) {
+    sum = sum + i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
