d7d9f62dd1a172afb096d74d99c782ec02008dc3
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-22 14:06:05 +0800

    Add logic and validation to fibonacci() function

--- javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+  if (number < 0) return 'OOPS';
+
+  let arrFibonacci = [];
+
+  for (i = 0 ; i < number ; i++) {
+    if(arrFibonacci.length < 2) {
+      arrFibonacci.push(1);
+    } else {
+      let sumOfLastPair = arrFibonacci[arrFibonacci.length-1]+arrFibonacci[arrFibonacci.length-2] 
+      arrFibonacci.push(sumOfLastPair);
+    }
+  }
+
+  return arrFibonacci[number-1];
 
 };
 
