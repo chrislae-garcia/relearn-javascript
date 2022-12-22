5d6eb2a960d5545fc2ee0cb07d3e6bb8f2b190b5
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-22 13:17:49 +0800

    Add logic and validation for palindromes() function

--- javascript-exercises-main/09_palindromes/palindromes.js
+++ javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  let lowerCaseString = string.toLowerCase();
+  let originalString = '';
+
+  for (i = 0; i < string.length; i++) {
+    let charCode = lowerCaseString.charCodeAt(i);
+    if (charCode >= 97 && charCode <= 122) {
+      originalString+=lowerCaseString.substr(i,1);
+    }
+  }
+
+  let reversedString = originalString.split('').reverse().join('');
+
+  return reversedString == originalString;
 };
 
 // Do not edit below this line
