620d8480f0a7972bc2c978e704d1db00d6031400
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-22 19:29:22 +0800

    Add logic to find the oldest person

--- javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  
+  people.forEach(element => {
+    if(element.yearOfDeath==undefined) {
+      element.yearOfDeath = new Date().getFullYear();
+    }
+  });
 
+  const oldest = people.sort((a, b) => {
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  })
+
+  return oldest[0];
 };
 
 // Do not edit below this line
