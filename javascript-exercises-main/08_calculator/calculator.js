4f8a1e23868abc5aa8304a0650d549d3f0751b18
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-12-22 12:33:54 +0800

    Add calculations in calculator.js file

--- javascript-exercises-main/08_calculator/calculator.js
+++ javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+
+  array.forEach(element => {
+    sum += element;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  let product = 1;
 
+  array.forEach(element => {
+    product *= element;
+  });
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  
+  let total = 1;
+  
+  while (number > 0) {
+    total *= number;
+    number--;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
